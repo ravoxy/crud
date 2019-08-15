aa01a5c47cb0d265deb46853f3010f49974f2465
Giovani Pereira <gleite@mpmt.mp.br>
2019-08-15 15:40:14 -0400

    configurando Swagger

--- src/main/java/br/com/icecode/crud/config/SwaggerConfigurations.java
+++ src/main/java/br/com/icecode/crud/config/SwaggerConfigurations.java
@@ -17,39 +17,17 @@ public class SwaggerConfigurations {
     public Docket detalheApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("br.com.icecode.financeiro"))
+                .apis(RequestHandlerSelectors.basePackage("br.com.icecode.crud"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(this.informacoesApi().build());
-//                .ignoredParameterTypes(Usuario.class)
-//                .globalOperationParameters(Arrays.asList(
-//                        new ParameterBuilder()
-//                                .name("Authorization")
-//                                .description("Header para token JWT")
-//                                .modelRef(new ModelRef("string"))
-//                                .parameterType("header")
-//                                .required(false)
-//                                .build()));
     }
 
     private ApiInfoBuilder informacoesApi() {
 
         return new ApiInfoBuilder()
-                .title("Api-Financeiro")
-                .description("Api para gestão financeira.")
+                .title("Api-CRUD")
+                .description("Api para CRUD.")
                 .version("1.0");
-//                .termsOfServiceUrl("Termo de uso: Deve ser usada para estudos.")
-//                .license("Licença - Open Source")
-//                .licenseUrl("http://www.ciceroednilson.com.br")
-//                .contact(this.contato());
-
     }
-
-//    private Contact contato() {
-//
-//        return new Contact(
-//                "Cícero Ednilson",
-//                "http://www.ciceroednilson.com.br",
-//                "ciceroednilson@gmail.com");
-//    }
 }
