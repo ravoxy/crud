f75408f11fd8a0ac7d7fc16cb5d41a8b2f14c0c0
Giovani Pereira <gleite@mpmt.mp.br>
2019-08-19 15:34:20 -0400

    Corrigindo bug de cors

--- src/main/java/br/com/icecode/crud/cors/CorsFilter.java
+++ src/main/java/br/com/icecode/crud/cors/CorsFilter.java
@@ -26,10 +26,10 @@ public class CorsFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) resp;
 
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:8000");
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
         response.setHeader("Access-Control-Allow-Credentials", "true");
 
-        if ("OPTIONS".equals(request.getMethod()) && "http://localhost:8000".equals(request.getHeader("Origin"))) {
+        if ("OPTIONS".equals(request.getMethod()) && "http://localhost:4200".equals(request.getHeader("Origin"))) {
             response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
             response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
             response.setHeader("Access-Control-Max-Age", "3600");
