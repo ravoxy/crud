bcd8ff80c28a8d2d4d369cfca561e7b37f92e3dd
Giovani Pereira <giovani_cuiaba@hotmail.com>
2019-08-27 19:45:23 -0400

    criando buscar pelo id

--- src/main/java/br/com/icecode/crud/resource/UsuarioResource.java
+++ src/main/java/br/com/icecode/crud/resource/UsuarioResource.java
@@ -1,28 +1,18 @@
 package br.com.icecode.crud.resource;
 
-import java.util.List;
-
-import javax.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
 import br.com.icecode.crud.event.RecursoCriadoEvent;
 import br.com.icecode.crud.model.Usuario;
 import br.com.icecode.crud.repository.UsuarioRepository;
 import br.com.icecode.crud.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -44,6 +34,12 @@ public class UsuarioResource {
     @GetMapping
     public List<Usuario> listar() {
         return usuarioRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> buscarPeloId(@PathVariable Long id) {
+        Usuario usuario = usuarioRepository.findById(id).orElse(null);
+        return usuario != null ? ResponseEntity.ok(usuario) : ResponseEntity.notFound().build();
     }
 
     @PostMapping
